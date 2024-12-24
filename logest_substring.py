44e67d954af717f35085136c0dcbce196d4ef6b9
Pawan <avvaripawan3@gmail.com>
2024-12-24 13:22:18 -0500

    longest_substring

---
+++ logest_substring.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l = 0
+
+        charSte = set()
+
+        for r in range(len(s)):
+
+            while s[r] in set:
+
+                charset.remove(s[l])
+                l +=1
+
+            charset.adds[r]
+
+            res = max(res, r -l + 1)    
+
+        return  res
+
+
+
+
+
+
+
+charSet = {}
